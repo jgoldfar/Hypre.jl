14fe0d9fd3d21ae3864893e9e0c201d8d352963e
Max Goldfarb <jgoldfar@gmail.com>
2015-01-27 16:32:23 -0500

    Added an include file for the libhypre_h file which defines MPI constants based on the file mpi_h_min.h added in this commit. YMMV; depends on MPI version (no way to get these (Apparently) automatically from MPI.jl. Will keep looking into that...

---
+++ src/libhypre_mpi_h.jl
@@ -0,0 +1,39 @@
+typealias MPI_Datatype Cint
+typealias MPI_Comm Cint
+typealias MPI_Request Cint;
+
+# Skipping MacroDefinition:
+const MPI_CHAR = 0x4c000101
+# Skipping MacroDefinition:
+const MPI_SIGNED_CHAR = 0x4c000118
+# Skipping MacroDefinition:
+const MPI_UNSIGNED_CHAR = 0x4c000102
+# Skipping MacroDefinition:
+const MPI_BYTE = 0x4c00010d
+# Skipping MacroDefinition:
+const MPI_WCHAR = 0x4c00040e
+# Skipping MacroDefinition:
+const MPI_SHORT = 0x4c000203
+# Skipping MacroDefinition:
+const MPI_UNSIGNED_SHORT = 0x4c000204
+# Skipping MacroDefinition:
+const MPI_INT = 0x4c000405
+# Skipping MacroDefinition:
+const MPI_UNSIGNED = 0x4c000406
+# Skipping MacroDefinition:
+const MPI_LONG = 0x4c000807
+# Skipping MacroDefinition:
+const MPI_UNSIGNED_LONG = 0x4c000808
+# Skipping MacroDefinition:
+const MPI_FLOAT = 0x4c00040a
+# Skipping MacroDefinition:
+const MPI_DOUBLE = 0x4c00080b
+# Skipping MacroDefinition:
+const MPI_LONG_DOUBLE = 0x4c00100c
+# Skipping MacroDefinition:
+const MPI_LONG_LONG_INT = 0x4c000809
+# Skipping MacroDefinition:
+const MPI_UNSIGNED_LONG_LONG = 0x4c000819
+
+const hypre_MPI_Datatype = MPI_Datatype
+const hypre_MPI_Request = MPI_Request
