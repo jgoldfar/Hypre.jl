7453da4f83c3276d832f9f2dc3dfdc561ba08521
Max Goldfarb <jgoldfar@gmail.com>
2015-01-27 13:04:27 -0500

    Build script now runs make install to the correct location/prefix. Ignoring more build output (as well as deps file.)

--- deps/build.jl
+++ deps/build.jl
@@ -9,14 +9,15 @@ hyprefilebase = "hypre-$(hyprever)"
 provides(Sources,
          URI("http://ftp.mcs.anl.gov/pub/petsc/externalpackages/$(hyprefilebase).tar.gz"),
          libhypre)
-println(BinDeps.depsdir(libhypre))
+#println(BinDeps.depsdir(libhypre))
+#println(BinDeps.libdir(libhypre))
 prefix = joinpath(BinDeps.depsdir(libhypre), "usr")
 srcdir = joinpath(BinDeps.depsdir(libhypre), "src", hyprefilebase)
 srclibdir = joinpath(srcdir, "lib")
 cmakebuilddir = joinpath(srcdir, "src", "cmbuild")
 
-println("Installing Hypre source to: ", srcdir)
-println("Installing Hypre Cmake build directory: ", cmakebuilddir)
+#println("Installing Hypre source to: ", srcdir)
+#println("Installing Hypre Cmake build directory: ", cmakebuilddir)
 
 
 provides(SimpleBuild, (@build_steps begin
@@ -25,9 +26,10 @@ provides(SimpleBuild, (@build_steps begin
                          @build_steps begin
                            ChangeDirectory(cmakebuilddir)
                            `cmake -DHYPRE_SHARED:BOOL=ON ..`
+`cmake -DHYPRE_INSTALL_PREFIX:PATH=$prefix ..`
                            `cmake -L ..`
                            `make`
-                           `cp libHYPRE.so $srclibdir`
+`make install`
                          end
                        end), libhypre)
 
