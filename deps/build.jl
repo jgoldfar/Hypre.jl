778f13a5267d9195719b3888414553c3a5a31a51
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-02-22 17:57:41 -0500

    Fixed build on OSX

--- deps/build.jl
+++ deps/build.jl
@@ -9,28 +9,40 @@ hyprefilebase = "hypre-$(hyprever)"
 provides(Sources,
          URI("http://ftp.mcs.anl.gov/pub/petsc/externalpackages/$(hyprefilebase).tar.gz"),
          libhypre)
-#println(BinDeps.depsdir(libhypre))
-#println(BinDeps.libdir(libhypre))
-prefix = joinpath(BinDeps.depsdir(libhypre), "usr")
-srcdir = joinpath(BinDeps.depsdir(libhypre), "src", hyprefilebase)
-srclibdir = joinpath(srcdir, "lib")
+
+prefix = BinDeps.usrdir(libhypre)
+srcrootdir = BinDeps.srcdir(libhypre)
+srcdir = joinpath(srcrootdir, hyprefilebase)
 cmakebuilddir = joinpath(srcdir, "src", "cmbuild")
 
-#println("Installing Hypre source to: ", srcdir)
-#println("Installing Hypre Cmake build directory: ", cmakebuilddir)
-
-
-provides(SimpleBuild, (@build_steps begin
-                         GetSources(libhypre)
-                         CreateDirectory(srcdir)
-                         @build_steps begin
-                           ChangeDirectory(cmakebuilddir)
-                           `cmake -DHYPRE_SHARED:BOOL=ON ..`
-                           `cmake -DHYPRE_INSTALL_PREFIX:PATH=$prefix ..`
-                           `cmake -L ..`
-                           `make`
-                           `make install`
-                         end
-                       end), libhypre, os=:Unix)
+@linux_only begin
+  provides(SimpleBuild, (@build_steps begin
+                           GetSources(libhypre)
+                           CreateDirectory(srcdir)
+                           @build_steps begin
+                             ChangeDirectory(cmakebuilddir)
+                             `cmake -DHYPRE_SHARED:BOOL=ON ..`
+                             `cmake -DHYPRE_INSTALL_PREFIX:PATH=$prefix ..`
+                             `cmake -L ..`
+                             `make`
+                             `make install`
+                           end
+                         end), libhypre)
+end
+@osx_only begin
+  provides(SimpleBuild, (@build_steps begin
+                           GetSources(libhypre)
+                           CreateDirectory(srcdir)
+                           @build_steps begin
+                             ChangeDirectory(cmakebuilddir)
+                             `cmake -DHYPRE_SHARED:BOOL=ON ..`
+                             `cmake -DHYPRE_SEQUENTIAL:BOOL=ON ..`
+                             `cmake -DHYPRE_INSTALL_PREFIX:PATH=$prefix ..`
+                             `cmake -L ..`
+                             `make`
+                             `make install`
+                           end
+                         end), libhypre)
+end
 
 @BinDeps.install [:libhypre => :libhypre]
