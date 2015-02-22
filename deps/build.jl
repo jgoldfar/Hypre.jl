186cf0dfa80d151d98e83f5f188286590a0f558d
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-02-22 17:59:26 -0500

    Added basic support for "flags" in build.jl script (really more useful for debug/testing purposes)

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,7 @@ using BinDeps
 
 @BinDeps.setup
 
+## Start defining dependency
 libhypre = library_dependency("libhypre", aliases=["libhypre", "libHYPRE"])
 hyprever="2.9.0b"
 hyprefilebase = "hypre-$(hyprever)"
@@ -10,11 +11,47 @@ provides(Sources,
          URI("http://ftp.mcs.anl.gov/pub/petsc/externalpackages/$(hyprefilebase).tar.gz"),
          libhypre)
 
+## Define paths used later.
 prefix = BinDeps.usrdir(libhypre)
 srcrootdir = BinDeps.srcdir(libhypre)
 srcdir = joinpath(srcrootdir, hyprefilebase)
 cmakebuilddir = joinpath(srcdir, "src", "cmbuild")
 
+## Add support for basic "build flags"
+if !isempty(ARGS)
+  if ARGS[1] == "clean"
+    for dir in [BinDeps.srcdir(libhypre),
+                ]
+      println("Removing ", dir)
+      try
+        rm(dir, recursive = true)
+      catch v
+        println("Already gone")
+      end
+    end #for
+    exit(0)
+  end #if
+  if ARGS[1] == "clean-all"
+    for dir in [BinDeps.srcdir(libhypre),
+                prefix,
+                BinDeps.downloadsdir(libhypre),
+                ]
+      println("Removing ", dir)
+      try
+        rm(dir, recursive = true)
+      catch v
+        println("Already gone")
+      end
+    end #for
+    exit(0)
+  end #if
+  #   TODO: After fixing/finding MPI support on OSX, add an option to
+  #   build with MPI support.
+
+end
+
+
+## Build steps
 @linux_only begin
   provides(SimpleBuild, (@build_steps begin
                            GetSources(libhypre)
@@ -45,4 +82,5 @@ end
                          end), libhypre)
 end
 
+## Install
 @BinDeps.install [:libhypre => :libhypre]
