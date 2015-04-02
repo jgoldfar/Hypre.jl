9d92e434e221628398a246eb21af1b31449e12ad
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-04-02 17:02:43 -0400

    Looking to whether export status of libhypre is causing the failed build.

--- src/Hypre.jl
+++ src/Hypre.jl
@@ -1,9 +1,10 @@
 # Hypre.jl: Wrapper around the Hypre library (http://acts.nersc.gov/hypre/)
 module Hypre
 if isfile(joinpath(dirname(dirname(@__FILE__)), "deps", "deps.jl"))
-    include(joinpath(dirname(dirname(@__FILE__)), "deps", "deps.jl"))
+  include(joinpath(dirname(dirname(@__FILE__)), "deps", "deps.jl"))
+  export libhypre
 else
-    error("Hypre not properly installed. Please run Pkg.build(\"Hypre\")")
+  error("Hypre not properly installed. Please run Pkg.build(\"Hypre\")")
 end
 
 using MPI
